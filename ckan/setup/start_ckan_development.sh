8127aead4ae241f614f3ac14d288b3e56edfeed9
Gerben Neven <gerbyzation@me.com>
2018-01-23 11:44:10 +0000

    update start_ckan_development.sh to load test settings into test-core.ini

--- ckan/setup/start_ckan_development.sh
+++ ckan/setup/start_ckan_development.sh
@@ -33,6 +33,15 @@ done
 echo "Loading the following plugins: $CKAN__PLUGINS"
 paster --plugin=ckan config-tool $CKAN_INI "ckan.plugins = $CKAN__PLUGINS"
 
+# Update test-core.ini DB, SOLR & Redis settings
+echo "Loading test settings into test-core.ini"
+paster --plugin=ckan config-tool $SRC_DIR/ckan/test-core.ini "sqlalchemy.url = postgresql://ckan:ckan@db/ckan_test"
+paster --plugin=ckan config-tool $SRC_DIR/ckan/test-core.ini "ckan.datstore.write_url = postgresql://ckan:ckan@db/datastore_test"
+paster --plugin=ckan config-tool $SRC_DIR/ckan/test-core.ini "ckan.datstore.read_url = postgresql://datastore_ro:datastore@db/datastore_test"
+
+paster --plugin=ckan config-tool $SRC_DIR/ckan/test-core.ini "solr_url = http://solr:8983/solr/ckan"
+paster --plugin=ckan config-tool $SRC_DIR/ckan/test-core.ini "ckan.redis_url = redis://redis:6379/1"
+
 # Run the prerun script to init CKAN and create the default admin user
 python prerun.py
 
