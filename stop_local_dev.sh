50f2f42e18f010a8e821b4d85165fd981570932b
Laurence Watson <laurence.watson@gmail.com>
2022-01-17 10:45:53 +0000

    gitignore some local scripts

--- stop_local_dev.sh
+++ stop_local_dev.sh
@@ -8,6 +8,7 @@ sudo service tomcat9 stop
 # Start CKAN helper processes running under supervisorctl
 sudo supervisorctl stop ckan-datapusher:ckan-datapusher-00
 sudo supervisorctl stop ckan-worker:ckan-worker-00
+sudo supervisorctl stop ckan-uwsgi:ckan-uwsgi-00
 
 # Not this one because we want to run with CKAN command rather than full webserver
 # sudo supervisorctl start ckan-uwsgi:ckan-uwsgi-00
